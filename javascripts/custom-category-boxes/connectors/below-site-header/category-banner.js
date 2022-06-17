7061d6e96634b837be9bfe676f10a0fec8de19ae
Jarek Radosz <jradosz@gmail.com>
2022-06-18 01:13:37 +0200

    eslint

--- javascripts/custom-category-boxes/connectors/below-site-header/category-banner.js
+++ javascripts/custom-category-boxes/connectors/below-site-header/category-banner.js
@@ -1,14 +1,12 @@
 import { withPluginApi } from 'discourse/lib/plugin-api';
-import Component from "@ember/component";
-import { afterRender } from "discourse-common/utils/decorators";
 
 export default {
   setupComponent(args, component) {
     withPluginApi("0.1", api => {
 
-      api.onPageChange((url, title) => {
+      api.onPageChange((url) => {
         let show_banner = settings.show_banner;
-        let splitURL = url.split("/")
+        let splitURL = url.split("/");
         let html = document.getElementsByTagName("html")[0];
 
         if (splitURL[1] === "c") {
@@ -17,35 +15,31 @@ export default {
           let rnd = Math.floor(Math.random()*4);
           let bg = `${settings.category_background}-${rnd}`;
 
-          let c;
-
-          this.site.categories.forEach((cat) => {
+          let c = this.site.categories.find((cat) => {
             let name = cat.name.toLowerCase().replace(" ","-");
             if (name === categoryTitle.toLowerCase()) {
-              c = cat;
+              return true;
             }
-          })
+          });
 
-
-          html.classList.add("category-page-custom-banner")
+          html.classList.add("category-page-custom-banner");
 
           component.setProperties({
             show_banner,
-            title: c.name.replace(/^\w/, c => c.toUpperCase()),
+            title: c.name.replace(/^\w/, cat => cat.toUpperCase()),
             backgroundColor: `#${c.color}65`,
             backgroundImage: `url(${settings.theme_uploads[bg]})`,
             border: `1px solid #${c.color}`,
             boxShadow: `8px 8px 0 #${c.color}32`,
-          })
+          });
 
         } else {
-          html.classList.remove("category-page-custom-banner")
+          html.classList.remove("category-page-custom-banner");
           component.setProperties({
             show_banner: false
-          })
+          });
         }
-      })
-
-    })
+      });
+    });
   }
-}
+};
