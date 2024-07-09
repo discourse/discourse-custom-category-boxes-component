16f8550ca4d5202e46ce17c5d000c593233e409f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-07-09 12:58:45 +0200

    FIX: updates component to async categories (#23)
    When a site had more categories than what we preload by default, visiting a non-preloaded category page would raise an error:
    
    ```
    client-error-handler.js:125 [THEME 2 'Custom Category Boxes']
    TypeError: Cannot read properties of undefined (reading 'name')
        at category-banner.js:31:24
        at plugin-api.js:127:26
        at AppEvents.<anonymous> (plugin-api.js:736:50)
        at m (index.js:200:1)
        at AppEvents.trigger (evented.js:27:1)
        at page-tracker.js:41:17
        at invoke (backburner.js.js:282:1)
        at h.flush (backburner.js.js:197:1)
        at p.flush (backburner.js.js:358:1)
        at B._end (backburner.js.js:798:1)
        at B.end (backburner.js.js:589:1)
        at B._runExpiredTimers (backburner.js.js:905:1)
    reportToConsole	@	client-error-handler.js:125
    ```

---
+++ spec/system/category_banner_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe "DiscourseCustomCategoryBoxes - category banner", system: true do
+  fab!(:current_user) { Fabricate(:admin) }
+
+  let(:category_page) { PageObjects::Pages::Category.new }
+
+  context "when using lazy loaded categories" do
+    let(:theme) { Fabricate(:theme) }
+
+    let!(:component) { upload_theme_component(parent_theme_id: theme.id) }
+
+    before do
+      SiteSetting.lazy_load_categories_groups = "#{Group::AUTO_GROUPS[:everyone]}"
+      theme.set_default!
+      sign_in(current_user)
+    end
+
+    fab!(:category) { Fabricate(:category) }
+    fab!(:topic) { Fabricate(:topic, category: category) }
+    fab!(:post) { Fabricate(:post, topic: topic) }
+
+    it "works" do
+      visit("/")
+
+      find(".badge-category[data-category-id='#{category.id}']").click
+
+      expect(category_page).to have_css(".custom-category-banner-title", text: category.name)
+    end
+  end
+end
